e4420360a947f0ce3d11ecb4c39ae63826f41648
Patrick Reinhart <patrick@reini.net>
2019-10-05 07:23:39 +0200

    Adds custom client id

--- cinema2k/src/main/java/org/tweetwallfx/vdz/Main.java
+++ cinema2k/src/main/java/org/tweetwallfx/vdz/Main.java
@@ -60,7 +60,6 @@ public class Main extends Application {
     private static final Logger LOG = LogManager.getLogger(Main.class);
     private static final Logger LOGGER = LogManager.getLogger(STARTUP);
 
-    private final UUID uuid = UUID.randomUUID();
     private final AtomicReference<Integer> exitCode = new AtomicReference<>();
 
     @Override
@@ -123,8 +122,9 @@ public class Main extends Application {
         Properties systemProperties = System.getProperties();
         for (;;) {
             String broker = systemProperties.getProperty("mqtt.url", "tcp://127.0.0.1:1883");
+            String clientId = systemProperties.getProperty("mqtt.client.id", UUID.randomUUID().toString());
             try (MqttClientPersistence persistence = new MemoryPersistence();
-                    MqttClient sampleClient = new MqttClient(broker, uuid.toString(), persistence)) {
+                    MqttClient sampleClient = new MqttClient(broker, clientId, persistence)) {
                 MqttConnectOptions connOpts = new MqttConnectOptions();
                 connOpts.setCleanSession(true);
                 connOpts.setConnectionTimeout(0);
@@ -138,16 +138,17 @@ public class Main extends Application {
                 sampleClient.connect(connOpts);
                 LOG.info("Connection established");
 
-                sampleClient.subscribe("tweetwall/action/#", this::handleMqttMessage);
+                sampleClient.subscribe("tweetwall/action/#", (t, m) -> handleMqttMessage(clientId, t, m));
                 for (;;) {
-                    sampleClient.publish("tweetwall/state/" + uuid, message("alive"));
-                    Integer rc = exitCode.get();
+                    sampleClient.publish("tweetwall/state/" + clientId, message("alive"));
+                    Integer rc = exitCode.getAndSet(null);
                     if (rc != null) {
-                        sampleClient.publish("tweetwall/" + uuid, message("stopping"));
+                        sampleClient.publish("tweetwall/state/" + clientId, message("stopping"));
                         LOG.warn("Going to exit with rc {}", rc);
                         sampleClient.disconnect();
-                        stop();
-//                        System.exit(rc.intValue());
+                        Platform.exit();
+                        // System.exit(rc.intValue());
+                        return;
                     }
 
                     TimeUnit.SECONDS.sleep(5);
@@ -166,10 +167,9 @@ public class Main extends Application {
         return message;
     }
 
-    void handleMqttMessage(String topic, MqttMessage message) {
+    void handleMqttMessage(String clientId, String topic, MqttMessage message) {
         String payload = new String(message.getPayload(), StandardCharsets.UTF_8);
-        LOG.warn("Unkown payload '{}' for topic {}", payload, topic);
-        if (topic.equals("tweetwall/action/" + uuid)) {
+        if (topic.equals("tweetwall/action/" + clientId)) {
             switch (payload) {
             case "stop": {
                 exitCode.set(Integer.valueOf(0));
@@ -181,6 +181,8 @@ public class Main extends Application {
             }
             default:
             }
+        } else {
+            LOG.warn("Unkown payload '{}' for topic {}", payload, topic);
         }
     }
 
